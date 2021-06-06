3cbdcc782da7eff4933054d423bd4202eecb9235
MeghaShyam26 <meghashyam138@gmail.com>
2021-06-06 18:10:26 +0530

    Main code and constants

---
+++ app.py
@@ -0,0 +1,92 @@
+import flask
+import smtplib
+import requests
+import json
+from datetime import datetime, timedelta
+import time
+from constants import constants
+import uuid
+app = flask.Flask(__name__)
+
+''' GET request '''
+# headers
+headers = {
+    'Content-type': 'application/json',
+}
+
+
+# url to hit
+today = (str(datetime.now())[:10]).split('-')
+url_1 = "https://cdn-api.co-vin.in/{}?pincode={}&date={}-{}-{}".format(constants.pin_code_url, constants.pincode, today[2], today[1], today[0])
+
+today = (str(datetime.now() + timedelta(days=1)).split(' ')[0]).split('-')
+url_2 = "https://cdn-api.co-vin.in/{}?pincode={}&date={}-{}-{}".format(constants.pin_code_url, constants.pincode, today[2], today[1], today[0])
+
+today = (str(datetime.now() + timedelta(days=2)).split(' ')[0]).split('-')
+url_3 = "https://cdn-api.co-vin.in/{}?pincode={}&date={}-{}-{}".format(constants.pin_code_url, constants.pincode, today[2], today[1], today[0])
+
+# request type
+method = "GET"
+
+# flag to check if requests should be made
+operation_check = True
+
+def operation():
+
+    # var to load the metadata if the slots are available to send a mail
+    text = ''
+
+    for url in [url_1, url_2, url_3]:
+        # making an api request
+        response =  requests.request(method, url)
+        result = json.loads(response.text)
+        sessions = result["sessions"]
+        # checking if any hospital has doses for the appropriate dose number and age
+        for session in sessions:
+            if (session['min_age_limit']==constants.req_age):
+                if (session['available_capacity_dose{}'.format(constants.req_slot)]>0):
+                    text = text + 'hospital:{}, address:{}, date:{}, age_limit:{}, available dose1 count:{}, available dose2 count:{}, fee:{}, vaccine:{}, slots:{}.  '.format(session['name'], session['address'], session['date'], session['min_age_limit'], session['available_capacity_dose1'], session['available_capacity_dose2'], session['fee'], session['vaccine'], session['slots'])
+    
+    # checking the text is empty to see if the slots are available to send a notification via email
+    if (text != ''):
+        # message for the email
+        text = text + '      navigate to  https://selfregistration.cowin.gov.in/ and book the slot'
+        # subject for the email
+        subject = 'covid vaccine slot update id:' + str(uuid.uuid4())
+        # combined message and subject for the email
+        message = 'Subject: {}\n\n{}'.format(subject, text)
+        # logging into the from email id
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server.login(constants.username, constants.password)
+        # enter a valid to address
+        server.sendmail(constants.username,"<to address>", message)
+        server.quit()
+        print('[INFO] Mail sent')
+        return 'exists'
+    return 'not-exists'
+
+# notify route points to the notification route
+@app.route('/notify', methods=['GET'])
+def main():
+    global operation_check
+
+    operation_check = True
+    while operation_check:
+        result = operation()
+        sleep_count = constants.sleep_availability_check
+        # if there are slots then we hit the url after slot_check numbered seconds
+        if (result=='exists'):
+            sleep_count = constants.sleep_availability_count_check
+        time.sleep(sleep_count)
+    return 'success'
+
+# stop route points to stop hitting the url
+@app.route('/stop', methods=['GET'])
+def stop_operations():
+    global operation_check
+    operation_check = False
+    return 'success'
+
+# enter a port number of choice if hosted locally or 8080 if posting via docker or other cloud/online service and make the host=0.0.0.0
+if __name__ == "__main__":
+    app.run(port = 8001)
---
+++ constants.py
@@ -0,0 +1,29 @@
+class constants:
+
+   # username
+   # this email id shouldn't have 2 factor authentication
+   username = "<enter a valid email id>"
+
+   # password
+   password = "<enter the appropriate password>"
+   
+   # required age
+   # change the age to 45 if required
+   req_age = 18
+   
+   # required slot number
+   # change the slot to 2 if u r looking for the second slot
+   req_slot = 1
+   
+   # pin code
+   # change the pincode to your area pincode or the interested one
+   pincode = 530040
+
+   # pin code api url
+   pin_code_url = 'api/v2/appointment/sessions/public/findByPin'
+
+   # sleep time for availability count
+   sleep_availability_count_check = 60
+
+   # sleep time for availability
+   sleep_availability_check = 300
