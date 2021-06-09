c9834f24d89b196ea1423eaae3e190c82bc4ca2d
MeghaShyam26 <meghashyam138@gmail.com>
2021-06-09 18:33:51 +0530

    Added a list to take to email ids

--- constants.py
+++ constants.py
@@ -1,11 +1,15 @@
 class constants:
 
-   # username
+   # username, Type: String
    # this email id shouldn't have 2 factor authentication
    username = "<enter a valid email id>"
 
-   # password
+   # password, Type: String
    password = "<enter the appropriate password>"
+
+   # to email addresses => Enter a valid to email address and this email id need not disable the 2 step verification, Type: String
+   # to enter more than 1 email id append them spearated with a comma ex) ['example1@fmail.com', 'example2@outlook.com',...]
+   to_addresses = ['<--to email address-->']
    
    # required age
    # change the age to 45 if required
