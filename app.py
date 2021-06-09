3900de007f7210255f5acb8318a48b0f8f4dd97e
MeghaShyam26 <meghashyam138@gmail.com>
2021-06-09 18:14:01 +0530

    Changed logic to send only 1 notification when slots open for a hospital and increase the hatch rate based on the peak hour mentioned

--- app.py
+++ app.py
@@ -20,7 +20,12 @@ method = "GET"
 # flag to check if requests should be made
 operation_check = True
 
+# flag to check if a mail was sent for an availability
+mail_check = {}
+
 def operation():
+
+    global mail_check
 
     # var to load the metadata if the slots are available to send a mail
     text = ''
@@ -31,6 +36,11 @@ def operation():
         # changing the date according to the day change
         today = (str(datetime.now() + timedelta(days=day)).split(' ')[0]).split('-')
         
+        scraped_date = "{}-{}-{}".format(today[2], today[1], today[0])
+
+        if (scraped_date not in mail_check.keys()):
+            mail_check[scraped_date] = {}
+
         # parsing through all the pincodes mentioned
         for pincode in constants.pincode:
             url = "https://cdn-api.co-vin.in/{}?pincode={}&date={}-{}-{}".format(constants.pin_code_url, pincode, today[2], today[1], today[0])
@@ -40,12 +50,29 @@ def operation():
             result = json.loads(response.text)
             sessions = result["sessions"]
             
-            # checking if any hospital has doses for the appropriate dose number and age
+            # parsing through the sessions from the api
             for session in sessions:
+                
+                # checking if the age limit of this session is under the required age
                 if (session['min_age_limit']==constants.req_age):
-                    if (session['available_capacity_dose{}'.format(constants.req_slot)]>0):
-                        text = text + 'hospital:{}, address:{}, date:{}, age_limit:{}, available dose1 count:{}, available dose2 count:{}, fee:{}, vaccine:{}, slots:{}.  '.format(session['name'], session['address'], session['date'], session['min_age_limit'], session['available_capacity_dose1'], session['available_capacity_dose2'], session['fee'], session['vaccine'], session['slots'])
-    
+
+                    # checking if the hospital already exists, if not append it to the dictionary
+                    if (session['name']+session['address'] not in mail_check[scraped_date].keys()):
+                        mail_check[scraped_date][session['name']+session['address']] = 0
+                    
+                        # checking if the available slots are greater than 0
+                        if (session['available_capacity_dose{}'.format(constants.req_slot)]!=0):
+                            
+                            # checking if already a notification has been sent fot the recent slot release of that hospital
+                            if (mail_check[scraped_date][session['name']+session['address']] == 0):
+                                
+                                mail_check[scraped_date][session['name']+session['address']] = 1
+                                text = text + 'hospital:{}, address:{}, date:{}, age_limit:{}, available dose1 count:{}, available dose2 count:{}, fee:{}, vaccine:{}, slots:{}.  '.format(session['name'], session['address'], session['date'], session['min_age_limit'], session['available_capacity_dose1'], session['available_capacity_dose2'], session['fee'], session['vaccine'], session['slots'])
+                                
+                        # if the available slots are 0 for that hospital then the flag is made 0 to send notification on fresh allotment
+                        else:
+                            mail_check[scraped_date][session['name']+session['address']] = 0
+                            
     # checking the text is empty to see if the slots are available to send a notification via email
     if (text != ''):
         # message for the email
@@ -71,11 +98,13 @@ def main():
 
     operation_check = True
     while operation_check:
-        result = operation()
-        sleep_count = constants.sleep_availability_check
-        # if there are slots then we hit the url after slot_check numbered seconds
-        if (result=='exists'):
-            sleep_count = constants.sleep_availability_count_check
+        # extarcting the current hour
+        hour = int((str(datetime.now()).split(' ')[-1]).split(':')[0])
+        operation()
+        sleep_count = constants.sleep_non_peak_time
+        # checking if the current hour is peak hour or not
+        if (hour >= constants.start_time and hour <= constants.end_time):
+            sleep_count = constants.sleep_peak_time
         time.sleep(sleep_count)
     return 'success'
 
